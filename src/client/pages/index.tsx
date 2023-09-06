192001ac60fcc5c52cb12c5b9dbd596cd5687cdd
KishoreTorlakonda Jum <kishore.torlakonda@jumbaya.com>
2023-09-06 10:27:12 +0530

    removed unused imports

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles, Theme, withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
